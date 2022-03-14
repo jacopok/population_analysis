2f474530b39fa88586f54c345b96d6d09a3c54c4
jacopok <jacopok@gmail.com>
2022-03-15 00:51:09 +0100

    various improvement with plots

--- jacopo/mass_distribution.py
+++ jacopo/mass_distribution.py
@@ -1,55 +1,85 @@
 from population_analysis.utils import read_file
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from matplotlib.cm import ScalarMappable
 from scipy.stats import gaussian_kde
 from pathlib import Path
 
 DATA_PATH = Path(__file__).parent / 'data'
+Zs = ['1e-4', '2e-4', '6e-4', '11e-4', '2e-3', '36e-4', '63e-4', '112e-4', '2e-2']
 
-def plot_contours(mass_1, mass_2, color):
+
+def plot_mass_contours(mass_1, mass_2, color):
     masses_1, masses_2 = [], []
     for m1, m2 in zip(mass_1, mass_2):
         if m2 > m1:
             m1, m2 = m2, m1
         masses_1.append(m1)
         masses_2.append(m2)
+    plot_contours(masses_1, masses_2, color)
 
-    kde = gaussian_kde(np.vstack((masses_1, masses_2)))
+def plot_total_mass_ratio_contours(mass_1, mass_2, color):
+    total_masses = mass_1 + mass_2
+    mass_ratios = np.maximum(mass_1 / mass_2, mass_2 / mass_1)
     
-    M1 = np.linspace(min(masses_1), max(masses_1), num=200)
-    M2 = np.linspace(min(masses_2), max(masses_2), num=200)
+    plot_contours(total_masses, mass_ratios, color)
+
+def plot_contours(var_1, var_2, color):
+
+    kde = gaussian_kde(np.vstack((var_1, var_2)))
     
-    m1_grid, m2_grid = np.meshgrid(M1, M2)
+    V1 = np.linspace(min(var_1), max(var_1), num=200)
+    V2 = np.linspace(min(var_2), max(var_2), num=200)
     
-    positions = np.vstack([m1_grid.ravel(), m2_grid.ravel()])
+    v1_grid, v2_grid = np.meshgrid(V1, V2)
+    
+    positions = np.vstack([v1_grid.ravel(), v2_grid.ravel()])
     kde_values = kde(positions)
     
-    mass_ratio_mask = m1_grid.ravel() < m2_grid.ravel()
+    mass_ratio_mask = v1_grid.ravel() < v2_grid.ravel()
     kde_values[mass_ratio_mask] = 0
     
     plt.contour(
-        m1_grid, 
-        m2_grid, 
-        kde_values.reshape(m1_grid.shape), 
-        levels=2, 
-        colors=['white', color, color, color],
-        linestyles=[':', '--', '-'],
+        v1_grid, 
+        v2_grid, 
+        kde_values.reshape(v1_grid.shape), 
+        levels=1, 
+        colors=['white', color, color],
+        linestyles=[':', '-', '-'],
     )
 
 if __name__ == '__main__':
-    cmap = plt.get_cmap('viridis')
-
-    data = read_file(DATA_PATH / 'mergers_Z=2e-4.out')
-    plot_contours(data['m1'], data['m2'], cmap(.2))
+    cmap = plt.get_cmap('inferno')
     
+    # eventually:
+    # [0.0002    , 0.00035566, 0.00063246, 0.00112468, 0.002     ,
+    #  0.00355656, 0.00632456, 0.01124683, 0.02      ]
+    float_Zs = [float(Z) for Z in Zs]
+    norm = LogNorm(vmin = min(float_Zs), vmax=max(float_Zs)*1.5)
 
-    data = read_file(DATA_PATH / 'mergers_Z=2e-3.out')
-    plot_contours(data['m1'], data['m2'], cmap(.5))
+    for Z in Zs:
+        data = read_file(DATA_PATH / f'mergers_Z={Z}.out')
+        plot_total_mass_ratio_contours(data['m1'], data['m2'], cmap(norm(float(Z))))
 
-    data = read_file(DATA_PATH / 'mergers_Z=2e-2.out')
-    plot_contours(data['m1'], data['m2'], cmap(.8))
+    plt.colorbar(ScalarMappable(cmap=cmap, norm=norm), label='Metallicity $Z$')
+
+    plt.ylim(1, 2.2)
+    plt.xlabel("Total mass [$M_{\odot}$]")
+    plt.ylabel("Mass ratio [1]")
     
+    plt.savefig('merger_mass_ratios.pdf')
+    plt.close()
+
+
+    for Z in Zs:
+        data = read_file(DATA_PATH / f'mergers_Z={Z}.out')
+        plot_mass_contours(data['m1'], data['m2'], cmap(norm(float(Z))))
+    
+    plt.colorbar(ScalarMappable(cmap=cmap, norm=norm), label='Metallicity $Z$')
+        
     plt.xlabel("Primary mass [$M_{\odot}$]")
     plt.ylabel("Secondary mass [$M_{\odot}$]")
     
     plt.savefig('merger_masses.pdf')
+    plt.close()
---
+++ jacopo/mass_gif.py
@@ -0,0 +1,63 @@
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
+import imageio
+
+from population_analysis.utils import read_file
+
+from mass_distribution import Zs
+
+DATA_PATH = Path(__file__).parent / 'data'
+
+def plot_frame(data):
+
+    total_masses = data['m1'] + data['m2']
+    mass_ratios = np.maximum(data['m1'] / data['m2'], data['m2'] / data['m1'])
+    
+    var_1, var_2 = total_masses, mass_ratios
+
+    kde = gaussian_kde(np.vstack((var_1, var_2)))
+    
+    V1 = np.linspace(min(var_1), max(var_1), num=200)
+    V2 = np.linspace(min(var_2), max(var_2), num=200)
+    
+    v1_grid, v2_grid = np.meshgrid(V1, V2)
+    
+    positions = np.vstack([v1_grid.ravel(), v2_grid.ravel()])
+    kde_values = kde(positions)
+    
+    mass_ratio_mask = v1_grid.ravel() < v2_grid.ravel()
+    kde_values[mass_ratio_mask] = 0
+    
+    plt.contourf(
+        v1_grid, 
+        v2_grid, 
+        kde_values.reshape(v1_grid.shape), 
+        levels=100,
+        cmap=plt.get_cmap('inferno')
+    )
+    plt.ylim(1, 2.2)
+    plt.xlim(3.9, 146)
+    plt.gca().set_facecolor('black')
+    plt.xlabel('Total mass [$M_{\odot}$]')
+    plt.ylabel('Mass ratio')
+    
+if __name__ == '__main__':
+
+    repeat_frame_times = 3
+
+    for i, Z in enumerate(Zs):
+        data = read_file(DATA_PATH / f'mergers_Z={Z}.out')
+        plot_frame(data)
+        plt.title(f'Z={float(Z):.1e}, N_mergers={len(data["m1"])}')
+        for j in range(repeat_frame_times):
+            plt.savefig(f'frames/{repeat_frame_times*i + j}.png')
+        plt.close()
+    
+    gif_name = 'mass_distribution'
+    
+    with imageio.get_writer(f'{gif_name}.gif', mode='I') as writer:
+        for filename in [f'frames/{i}.png' for i in range(repeat_frame_times*len(Zs))]:
+            image = imageio.imread(filename)
+            writer.append_data(image)
