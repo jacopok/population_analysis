9ed33fdd468298a0be3529496cfd2e70de0bb4d2
jacopok <jacopok@gmail.com>
2022-03-14 17:48:05 +0100

    Restructured package

--- population_analysis/utils.py
+++ population_analysis/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def read_file(filename: str) -> tuple[list[str], np.ndarray]:
+def read_file(filename: str) -> dict[str, np.ndarray]:
     """Read the given file and return
     its headers, as a list of strings, as well as the corresponding
     data as a numpy array.
@@ -12,35 +12,32 @@ def read_file(filename: str) -> tuple[list[str], np.ndarray]:
 
     Returns
     -------
-    headers: list[str]
-    numeric_data: np.ndarray
-        The shape of this array is (n_data, n_parameters).
+    data: dict[str, np.ndarray]
+        
     """
     
-    bad_strings = []
-    
-    def is_allowed(value: str):
-        
-        if value in bad_strings:
-            return False
-        
-        try:
-            float(value)
-            return True
-        except ValueError:
-            bad_strings.append(value)
-            return False
-
     data = []
 
     with open(filename, 'r') as f:
         lines = f.readlines()
 
-        header = [val for val in lines[0].split(' ') if val != '']
+        header = [val for val in lines[0].split(' ') if val]
 
-        data.extend([x for x in line.split(' ') if is_allowed(x)] for line in lines[1:])
+        data.extend([x for x in line.split(' ') if x] for line in lines[1:])
     
-    numeric_data = np.array(data, dtype=float)
     
-    return header, numeric_data
+    # only get the name of the column, without the index
+    # which is in square brackets
+    header = [name.split('[')[0] for name in header]
+
+    return_dict = {}
+
+    for i, name in enumerate(header):
+        
+        values = [row[i] for row in data]
+        try:
+            return_dict[name] = np.array(values, dtype=float)
+        except ValueError:
+            return_dict[name] = np.array(values, dtype=object)
     
+    return return_dict
