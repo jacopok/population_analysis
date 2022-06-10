a85016e91598b0d326c09b3e35e5a1c5b19cca76
jacopok <jacopok@gmail.com>
2022-06-10 21:04:02 +0200

    refactor: changing mergers to rows

--- population_analysis/input_manipulation.py
+++ population_analysis/input_manipulation.py
@@ -17,28 +17,28 @@ def read_popsyn(filename: str):
     
     with open(filename, 'r') as f:
         
-        n_mergers = next(f)
-        mergers = []
+        n_rows = next(f)
+        rows = []
         
         for line in f:
             # remove the newline at the end
             arr = re.split('\s+', line)[:-1]
             assert len(arr) == N_COLUMNS
-            mergers.append(arr)
+            rows.append(arr)
 
-    assert int(n_mergers) == len(mergers)
+    assert int(n_rows) == len(rows)
     
-    return np.array(mergers, dtype=str)
+    return np.array(rows, dtype=str)
 
-def write_popsyn(mergers: np.ndarray, filename: str):
+def write_popsyn(rows: np.ndarray, filename: str):
 
-    with open(filename, 'x') as f:
-        f.write(f'{len(mergers)}\n')
-        for merger in mergers:
+    with open(filename, 'w') as f:
+        f.write(f'{len(rows)}\n')
+        for merger in rows:
             f.write(" ".join(merger) + "\n")
 
-def change_metallicity(mergers: np.array, metallicity: float):
+def change_metallicity(rows: np.array, metallicity: float):
     
     met_string = f'{metallicity:.04f}'
     
-    mergers[:, PopsynColumns.stellar_metallicity] = met_string
+    rows[:, PopsynColumns.stellar_metallicity] = met_string
