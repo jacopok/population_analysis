a9bde6d5ec25823c267633d38539bb9cb79af1ed
jacopok <jacopok@gmail.com>
2022-06-22 17:31:55 +0200

    feat: select by comenv now running on all outputs

--- jacopo/select_by_comenv.py
+++ jacopo/select_by_comenv.py
@@ -1,17 +1,36 @@
 from pathlib import Path
+import numpy as np
 
 from population_analysis.utils import read_file, select_by_common_envelope
 
 base = Path(__file__).parent / 'data'
-em_file = base / 'evol_mergers.out'
-m_file = base / 'mergers.out'
 
-cols = read_file(em_file)
+Zs = []
+n_comenv = []
+n_tot = []
 
-ce, nce = select_by_common_envelope(cols)
+for Z in np.arange(1, 10_000) / 10_000:
+    
+    em_file = base / f'Z_{Z:.4f}evol_mergers.out'
+    m_file = base / f'Z_{Z:.4f}mergers.out'
 
-print(len(ce))
-print(len(nce))
+    try:
+        cols = read_file(em_file)
+    except FileNotFoundError:
+        continue
+    ce, nce = select_by_common_envelope(cols)
 
-merg = read_file(m_file)
-print(len(merg))
+    print(f'Z={Z}')
+    print(f'With common envelope: {len(ce)}')
+    print(f'Without common envelope: {len(nce)}')
+    
+    Zs.append(Z)
+    n_comenv.append(len(ce))
+    n_tot.append(len(ce) + len(nce))
+
+    merg = read_file(m_file)
+    if len(merg) != len(ce) + len(nce):
+        print('Merger number does not match!')
+    print(f'Mergers: {len(ce) + len(nce)}')
+    print()
+
