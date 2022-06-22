66f3907db3df13b5f8b48667eeaaa758ca496dc4
jacopok <jacopok@gmail.com>
2022-06-22 17:31:36 +0200

    feat: updated metallicity array

--- jacopo/data/run_mobse.py
+++ jacopo/data/run_mobse.py
@@ -1,7 +1,14 @@
-from population_analysis.utils import run_mobse_changing_metallicity
+import numpy as np
 from datetime import datetime
 
-metallicities = np.arange(1, 101) / 10_000
+from population_analysis.utils import run_mobse_changing_metallicity
+
+metallicities = np.concatenate((
+    np.arange(1, 20, step=1),
+    np.arange(20, 50, step=2),
+    np.arange(50, 200, step=4),
+    np.arange(200, 5000, step=8),
+)) / 10_000
 
 print(
     'Running with the following metallicities:'
